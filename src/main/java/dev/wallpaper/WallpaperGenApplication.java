6c7c1960d6d5a78feb81b08830e4aa5b74f6c3f7
david.rusycki <davidrusyck@gmail.com>
2023-11-17 22:49:56 -0300

    first commit

---
+++ src/main/java/dev/wallpaper/WallpaperGenApplication.java
@@ -0,0 +1,13 @@
+package dev.wallpaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallpaperGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallpaperGenApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/wallpaper/api/rest/MakeWallpaperController.java
@@ -0,0 +1,12 @@
+package dev.wallpaper.api.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/wallpaper/make")
+public class MakeWallpaperController {
+
+
+
+}
